85c639db8c5632bde9a36703120a187974b177c2
Alex Stone <stonealex145@gmail.com>
2022-02-20 13:40:38 -0600

    Go testing

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # learningGo
 This is my attempt to learn the go language
+
+
+## My first go program
+This is my best attempt to make a random number guessing game in go.
+I thiI think I failed and did not do good, but the good news is that the
+only place I can go from here is up.
+
+
+## First Impressions
+Go seems like a cool language based on all of the examples I have seen.
+It has a really clean syntax that I like, but there is a little weirdness.
+For examples what on earth does := copared to =. Overall, I am really 
+exicited to be picking up the langauge. I am excited to use it as pretty
+much just a faster version of python and I want to learn how OOP works 
+in go to, and maybe it is better than rust, who knows?
--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "math/rand"
+    "time"
+    "os"
+    "bufio"
+    "strconv"
+)
 
 func main() {
-	fmt.Println("Hi there !");
+    // Nice random number setup
+    rand.Seed(time.Now().UnixNano())
+    var random = rand.Intn(10);
+
+    for true {
+        // Read a line from the console
+        reader := bufio.NewReader(os.Stdin)
+        fmt.Print("Enter something: ");line, _ := reader.ReadString('\n')
+
+        // Check if that number is equal to a const
+        value, _  := strconv.Atoi(line) // _ is error
+        if random == value {
+            fmt.Println("You got it mister");
+            os.Exit(0)
+        } else {
+            fmt.Println("You did not get it mister");
+        }
+    }
 }
